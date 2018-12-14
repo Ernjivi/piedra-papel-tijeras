ceba4ffa72227b30ed841c72d22a6b80afa72589
Ernesto Jiménez <ernesto@bedu.org>
2018-12-13 20:50:52 -0600

    Finished example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Hola mundo!');
+// 0,1,2 | piedra, papel, tijer
+let results = ['piedra', 'papel', 'tijera']
+
+// Math.random()
+const rockPaperScissor = () => Math.floor(Math.random() * 3);
+// function rockPaperScissor(){...}
+
+let player1 = rockPaperScissor();
+let player2 = rockPaperScissor();
+
+console.log('Player1: ', results[player1], 'Player2: ', results[player2])
+
+if(player1 > player2 && player1 - player2 !== 2){
+   console.log('Gana Player1');
+} else if(player2 > player1 && player2 - player1 !== 2){
+  console.log('Gana Player2');
+} else{
+  console.log('Empate');
+}
+
+
+
+
